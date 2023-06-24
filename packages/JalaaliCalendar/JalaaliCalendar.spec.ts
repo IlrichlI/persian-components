7d73f53e2fb211cb973d4052953c3f0dc064d51e
ali aslani <aliaslani.mm@gmail.com>
2023-06-24 20:39:17 +0330

    fix(jalali-calendar): renaming

---
+++ packages/JalaaliCalendar/JalaaliCalendar.spec.ts
@@ -0,0 +1,75 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import { JalaaliCalendar } from './index'
+import { useJalali } from './useJalali'
+import { jalaliToGregorian } from './jalali'
+
+describe('JalaaliCalendar', () => {
+  it('renders properly', async () => {
+    const wrapper = mount(JalaaliCalendar)
+    await wrapper.find('button.day-btn').trigger('click')
+    const emitted = wrapper.emitted()
+    expect(emitted['on-select'].length).toBe(1)
+
+    expect((wrapper.emitted()['on-select'] as any)[0][0].jalali.jDay).toEqual(1)
+
+    expect(wrapper.findComponent(JalaaliCalendar).isVisible()).toBe(true)
+  })
+
+  it('next month', () => {
+    const { currentMonth, nextMonth } = useJalali()
+    const date = new Date(
+      currentMonth.value.getFullYear(),
+      currentMonth.value.getMonth() + 1,
+      currentMonth.value.getDate()
+    )
+    nextMonth()
+    expect(currentMonth.value.getMonth() === date.getMonth()).toBe(true)
+  })
+
+  it('previous month', () => {
+    const { currentMonth, previousMonth } = useJalali()
+    const date = new Date(
+      currentMonth.value.getFullYear(),
+      currentMonth.value.getMonth() - 1,
+      currentMonth.value.getDate()
+    )
+    previousMonth()
+    expect(currentMonth.value.getMonth() === date.getMonth()).toBe(true)
+  })
+
+  it('next Year', () => {
+    const { currentMonth, nextYear } = useJalali()
+    const date = new Date(
+      currentMonth.value.getFullYear() + 1,
+      currentMonth.value.getMonth(),
+      currentMonth.value.getDate()
+    )
+    nextYear()
+    expect(currentMonth.value.getFullYear() === date.getFullYear()).toBe(true)
+  })
+
+  it('previous Year', () => {
+    const { currentMonth, previousYear } = useJalali()
+    const date = new Date(
+      currentMonth.value.getFullYear() - 1,
+      currentMonth.value.getMonth(),
+      currentMonth.value.getDate()
+    )
+    previousYear()
+    expect(currentMonth.value.getFullYear() === date.getFullYear()).toBe(true)
+  })
+
+  it('select date', () => {
+    const { selectedDate, selectDate, getActiveDay, getJalaliDate } = useJalali()
+    selectDate(17)
+    const jalali = getJalaliDate(17)
+    const [year, month, d] = jalaliToGregorian(jalali.jYear, jalali.jMonth, jalali.jDay)
+    const gregorian = new Date(year, month - 1, d + 1)
+    expect(selectedDate.gregorian.getDate() === gregorian.getDate()).toBe(true)
+    expect(selectedDate.jalali.jDay === 17).toBe(true)
+    expect(getActiveDay(17)).toBe(true)
+    expect(getActiveDay(18)).toBe(false)
+  })
+})
