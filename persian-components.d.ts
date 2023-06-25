74695c66ee3ae50c1f19f9b8abcb9a4ade606339
ali aslani <aliaslani.mm@gmail.com>
2023-06-25 21:51:54 +0330

    fix: css

---
+++ persian-components.d.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+declare module 'persian-components' {
+  
+  import { defineComponent } from 'vue'
+  const component: ReturnType<typeof defineComponent<>>
+  const PersianCalendar = component
+
+  export default {
+    PersianCalendar
+  }
+
+}
