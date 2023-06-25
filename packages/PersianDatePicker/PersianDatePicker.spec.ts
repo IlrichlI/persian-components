ef9f145da10791951593854ba76852a2e58c8429
ali aslani <aliaslani.mm@gmail.com>
2023-06-25 11:22:00 +0330

    fix(date-picker): modal test

--- packages/PersianDatePicker/PersianDatePicker.spec.ts
+++ packages/PersianDatePicker/PersianDatePicker.spec.ts
@@ -2,10 +2,16 @@ import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import { PersianDatePicker } from './index'
+import { PersianComponents } from '../PersianComponents'
 
 describe('PersianDatePicker', () => {
   it('renders properly', async () => {
-    const wrapper = mount(PersianDatePicker)
+    const wrapper = mount(PersianDatePicker, {
+      global: {
+        plugins: [PersianComponents]
+      }
+    })
+
     expect(wrapper.findComponent(PersianDatePicker).isVisible()).toBe(true)
   })
 })
