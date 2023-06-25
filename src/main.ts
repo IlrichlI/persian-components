4cf8397cdf6f04c2861e3fb4b8ee49b3ae2ed5cd
ali aslani <aliaslani.mm@gmail.com>
2023-06-25 20:43:09 +0330

    fix(main): remove extras

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,3 @@ import '../public/main.css'
 
 export * from '../packages'
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import { PersianComponents } from '@/packages'
-
-const app = createApp(App)
-
-app.use(PersianComponents)
-
-app.mount('#app')
